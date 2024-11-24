56d48d6e8bc61b0cf0bb41692aef7bc6c0919f2d
Kaito Kid <>
2024-11-24 14:04:35 +0700

    frist commnit

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'index.html';
-    require_once 'about.html';
-    require_once 'contact.html';
-    require_once 'gallery.html';
-    require_once 'services.html';
-    require_once 'css/style.css';
-
-?>
